ec897788ba9fbd40d5be5cc257095e20110637ca
Rencie Bautista <rencie.bautista@yahoo.com>
2016-02-05 23:47:01 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Roll Up Automation</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    
+
+    <!-- Custom styles for this template -->
+    <link href="css/rasp.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+        <h2 class="text-center">Roll Up Automation</h2>
+      <form class="form-signin">
+        <button class="btn btn-lg btn-success btn-block" type="submit">Up</button>
+         <button class="btn btn-lg btn-danger btn-block" type="submit">Stop</button>
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Down</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
